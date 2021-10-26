7425e3b5fa65052fc41f66afe6d8f059ce99cc7e
Abebe-Nigusu <abebe.nigusu2021@gmail.com>
2021-10-26 12:04:57 -0500

    "my first githubeclipse commit"

---
+++ HelloWorld/src/helloworldabebe/Gitdemo1.java
@@ -0,0 +1,12 @@
+package helloworldabebe;
+
+public class Gitdemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("welcome to the Java world");
+		System.out.println("Demo");
+
+	}
+
+}
