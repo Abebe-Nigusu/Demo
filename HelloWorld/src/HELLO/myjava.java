79691c80badc506b4aa5284454506219e9838663
Abebe-Nigusu <abebe.nigusu2021@gmail.com>
2021-10-26 12:31:40 -0500

    "excersing java is crucial"

---
+++ HelloWorld/src/HELLO/myjava.java
@@ -0,0 +1,11 @@
+package HELLO;
+
+public class myjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print.out.pringln("excersiding java pays off");
+
+	}
+
+}
