daa447b082558c494f1a8e5b7cfe2e548d8a2c88
Abebe-Nigusu <abebe.nigusu2021@gmail.com>
2021-10-27 11:17:32 -0500

    "Another commit1"

---
+++ HelloWorldexercise/src/exercise1/class1forex1.java
@@ -0,0 +1,10 @@
+package exercise1;
+
+public class class1forex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println ("my java exercise project ");
+	}
+
+}
